c9ec77917e5151c31edc7cbab72ef0d537b4ceb5
KidiXDev <kidixdev@gmail.com>
2023-11-16 19:35:37 +0700

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
-import codecs
-import os
+from pathlib import Path
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Simple library to get Anime title and ID from MyAnimeList'
+
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / 'README.md').read_text()
 
 # Setting up
 setup(
@@ -12,9 +14,12 @@ setup(
     author="KidiXDev",
     author_email="<kidixdev@gmail.com>",
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
+    url='https://github.com/KidiXDev/pymal_basic',
     packages=find_packages(),
-    install_requires=['requests'],
+    license='MIT',
+    install_requires=[],
     keywords=['mal', 'myanimelist'],
     classifiers=[
         'Development Status :: 1 - Planning',
